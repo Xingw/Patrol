10d561de98dca50645bbcc05537863a13fbd0fb6
Jason <jeson.g@gmail.com>
2015-06-28 14:33:46 +0800

    add bugHD support
    add bugHD support
    increase the version number

--- app/src/main/java/com/xunce/patrol/util/MApplicatioin.java
+++ app/src/main/java/com/xunce/patrol/util/MApplicatioin.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.baidu.mapapi.SDKInitializer;
 import com.thinkland.sdk.android.JuheSDKInitializer;
 
+import im.fir.sdk.FIR;
+
 /**
  * Created by ssthouse on 2015/6/16.
  */
@@ -12,6 +14,8 @@ public class MApplicatioin extends Application {
 
     @Override
     public void onCreate() {
+        FIR.init(this);
+
         super.onCreate();
         SDKInitializer.initialize(getApplicationContext());
         //聚合数据初始化
